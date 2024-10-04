5bfa82786daef84faaf32b23c69669868a2f1788
mmcarpi <miguel@ime.usp.br>
2024-10-04 19:09:03 -0300

    Divide the dataset's text into sentences.

---
+++ sentences.py
@@ -0,0 +1,19 @@
+import polars as pl
+from polyglot.text import Text
+
+
+def split_into_sentences(raw_text):
+    return [sent.string for sent in Text(raw_text).sentences]
+
+
+df = pl.read_parquet("Data/caroldb.parquet")
+
+df = df.with_columns(
+    pl.col("text").map_elements(split_into_sentences, return_dtype=pl.List(pl.String))
+)
+df = df.filter(pl.col("text").list.len() > 0)
+df = df.explode("text")
+
+print(df)
+
+df.write_parquet("Data/caroldb-sentences.parquet")
