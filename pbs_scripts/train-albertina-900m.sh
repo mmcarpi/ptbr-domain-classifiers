525024e8eb7a039cc72826054f279ab7b09b12e6
mmcarpi <miguel@ime.usp.br>
2024-10-28 00:35:50 -0300

    pbs_scripts: scripts to train albertina-900m and bert-large

---
+++ pbs_scripts/train-albertina-900m.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+#PBS -q duasgpus
+#PBS -N albertina-900m-train
+#PBS -l nodes=1:ppn=32
+#PBS -e pbs_output/albertina-900m-train-errors
+#PBS -o pbs_output/albertina-900m-train-output
+#PBS -m abe
+
+unset CUDA_VISIBLE_DEVICES
+
+cd $PBS_O_WORKDIR
+
+source venv/bin/activate
+
+export HF_DATASETS_CACHE=venv/cache
+export HF_HOME=venv/cache
+
+CONFIG="Config/albertina-900m.json"
+NUM_EPOCHS=5
+EVAL_FREQUENCY=2
+LOG_FREQUENCY=10
+NUM_WORKERS=4
+
+torchrun --standalone --nproc_per_node=2 dist.py \
+	"$CONFIG" \
+	"$NUM_EPOCHS" \
+	"$EVAL_FREQUENCY" \
+	--eval_frequency="$LOG_FREQUENCY" \
+	--num_workers="$NUM_WORKERS"
---
+++ pbs_scripts/train-bert-large.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+#PBS -q umagpu
+#PBS -N bert-large-train
+#PBS -l nodes=1:ppn=16
+#PBS -e pbs_output/bert-large-train-errors
+#PBS -o pbs_output/bert-large-train-output
+#PBS -m abe
+
+unset CUDA_VISIBLE_DEVICES
+
+cd $PBS_O_WORKDIR
+
+source venv/bin/activate
+
+export HF_DATASETS_CACHE=venv/cache
+export HF_HOME=venv/cache
+
+CONFIG="Config/bert-large.json" 
+NUM_EPOCHS=5
+EVAL_FREQUENCY=2
+LOG_FREQUENCY=10
+NUM_WORKERS=4
+
+torchrun --standalone --nproc_per_node=1 dist.py \
+	"$CONFIG" \
+	"$NUM_EPOCHS" \
+	"$EVAL_FREQUENCY" \
+	--log_frequency="$LOG_FREQUENCY" \
+	--num_workers="$NUM_WORKERS" \
